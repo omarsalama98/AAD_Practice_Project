ebf6be00e79f97820b6c3177d201839d1874d2d6
Omar Salama <osalama710@gmail.com>
2020-08-30 23:52:48 +0200

    Removed Alert Dialog library (AwesomeDialog) and implemented the alert dialogs by myself.

--- app/src/main/java/com/example/o/aad_practice_project/SubmissionActivity.kt
+++ app/src/main/java/com/example/o/aad_practice_project/SubmissionActivity.kt
@@ -1,11 +1,15 @@
 package com.example.o.aad_practice_project
 
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.res.ResourcesCompat
 
 class SubmissionActivity : AppCompatActivity() {
 
@@ -31,11 +35,41 @@ class SubmissionActivity : AppCompatActivity() {
         submitButton.setOnClickListener {
 
             val mAlert = AlertDialog.Builder(this).create()
-            mAlert.setIcon(R.drawable.successfull_icon)
-            mAlert.setTitle("Are you sure?")
+            val mView = layoutInflater.inflate(R.layout.alert_dialog, null)
+            mAlert.setView(mView)
+            mAlert.setCancelable(true)
+            mAlert.window?.setBackgroundDrawable(
+                ResourcesCompat.getDrawable(resources, R.drawable.cornered_button, null)
+            )
+
+            mView.findViewById<TextView>(R.id.alert_message)?.text =
+                getString(R.string.are_you_sure_msg)
+            mView.findViewById<ImageView>(R.id.submission_cancel_button)?.setOnClickListener {
+                mAlert.dismiss()
+            }
+            mView.findViewById<Button>(R.id.submission_yes_button)?.setOnClickListener {
+                mAlert.dismiss()
+                val mAlertMessage = AlertDialog.Builder(this).create()
+                val messageView = layoutInflater.inflate(R.layout.alert_message, null)
+                mAlertMessage.setView(messageView)
+                mAlertMessage.window?.setBackgroundDrawable(
+                    ResourcesCompat.getDrawable(resources, R.drawable.cornered_button, null)
+                )
+
+                messageView.findViewById<TextView>(R.id.submission_response_message)?.text =
+                    getString(R.string.submission_successful)
+                messageView.findViewById<ImageView>(R.id.submission_response_image)
+                    ?.setImageDrawable(
+                        ResourcesCompat.getDrawable(resources, R.drawable.successful_icon, null)
+                    )
+                mAlertMessage.show()
+                Handler().postDelayed({
+                    mAlertMessage.dismiss()
+                    finish()
+                }, 3000)
+            }
             mAlert.show()
         }
-
     }
 
     private fun checkEmptyEntry(): Boolean {
